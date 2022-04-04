9c6a6488630b46c8f465890010e56c1b2850b89a
Mateusz Łuczak <luczakmmateusz@gmail.com>
2022-04-04 20:28:05 +0200

    feat: configuring linters

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Products from './components/products/products';
+import Header from './layout/components/header/header';
+import Footer from './layout/components/footer/footer';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const queryClient = new QueryClient();
+
+const App = () => (
+  <>
+    <Header />
+    <QueryClientProvider client={queryClient}>
+      <Products />
+    </QueryClientProvider>
+    <Footer />
+  </>
+);
 
 export default App;
